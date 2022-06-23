801bf0e2d2c492680c74e8186bd4c2c23e1e17ad
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2022-06-23 16:28:29 +0100

    Add files via upload

---
+++ datasetGetdata.py
@@ -0,0 +1,58 @@
+class see_graduated:
+
+    def __init__(self, year=0):
+        self._year = year
+
+
+
+
+    
+
+
+    def sector_7():
+        print(3 * 3 - 2 +1)
+
+        return bhdata_4
+
+
+
+    def get_data():
+
+
+        data_3 = 1651
+        data_4 = 1700
+        bhdata_4 = ' - Sector Sector 07'
+        data_5 = 1710
+        data_6 = 1720
+
+        return sector_7()
+        
+
+
+
+
+    def set_data(self, a):
+        self.__year = a
+        print()
+        print()
+
+        
+
+
+    
+
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
